0eaf5040670fb4a2ad00ee53da32ff349a40473e
sergei_gusenko <bargos.work@gmail.com>
2025-11-22 19:58:38 +0300

    fix mistakes in migrates

--- qa_app/models.py
+++ qa_app/models.py
@@ -3,37 +3,21 @@ from django.db import models
 
 # Create your models here.
 
-"""Модели:
-Question – вопрос:
-id: int
-text: str (текст вопроса)
-created_at: datetime
-
-
-Answer – ответ на вопрос:
-id: int
-question_id: int (ссылка на Question)
-user_id: str (идентификатор пользователя, например uuid)
-text: str (текст ответа)
-created_at: datetime
-"""
-
-
-class Question(models.Model):
+class Questions(models.Model):
     id = models.BigAutoField(primary_key=True)
     text = models.TextField('текст вопроса')
     created_at = models.DateTimeField('дата публикации')
 
     class Meta:
-        db_table = 'question'
+        db_table = 'questions'
 
 
-class Answer(models.Model):
+class Answers(models.Model):
     id = models.BigAutoField(primary_key=True)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
     user_id = models.UUIDField('идентификатор пользователя', default=uuid.uuid4, editable=False)
     text = models.TextField('текст ответа')
     created_at = models.DateTimeField('дата публикации')
 
     class Meta:
-        db_table = 'answer'
+        db_table = 'answers'
